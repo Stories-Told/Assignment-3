ab4ea9dfb70be17d76cfc7c4500d19d6d2ae145f
Stories-Told <54217603+Stories-Told@users.noreply.github.com>
2020-02-25 17:29:29 -0500

    Update Assignment_3.cpp

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+// Forward declarations
+double Calculate_Mean(double mean, double sum, double arr[]);
+double Find_Median(int j, int halfOfElement, double temp, double arr[], double median);
+
 int main()
 {
     // Input file
@@ -28,46 +32,48 @@ int main()
     // Array variable to store all the numbers in the
     // input file
     double arr[10000];
-    double sum;
-    // Counter variables
-    int n, m;
-    string line;
 
-    while(inFile)
+    // Takes half of the first element and
+    // then can add by 100 for each new elements half
+    int halfOfElement = 49;
+
+    // If statement to check and see if you
+    // are reading from the input file
+    if(inFile.is_open())
     {
-        for (int i = 0; i < 10000; i++)
+
+        // Variable for counting in the while loop
+        int i = 0;
+
+        // While loop to keep looping and store the numbers into
+        // an array
+        for (int i = 0; i < 100; i++)
         {
             inFile >> arr[i];
         }
     }
 
-    for (int i = 0; i < 100; i++)
+
+    // Output the first 100 numbers
+    for(int i = 0; i < 100; i++)
     {
-        cout << arr[i] << " ";
-
-        sum += arr[i] ;
+        cout << arr[i] << "\t";
     }
-
-    cout << "\nMean: " << fixed << setprecision(1) << sum / 100;
-
     cout << "\n\n";
 
-    sum = 0;
 
-    for (int i = 100; i < 200; i++)
-    {
+    // Variables for sorting function and finding median
+    double median;
+    int n,o,j;
+    int temp;
+    // Output the sorted first 100 numbers
+    cout << "Median: " << Find_Median(j,halfOfElement,temp,arr, median) << endl;
 
-        cout << arr[i] << " ";
-
-        sum += arr[i];
-    }
-
-    cout << "\nMean: " << fixed << setprecision(1) << sum / 100;
-
-
-
-
-
+    // Variables for calculating mean
+    double mean;
+    double sum;
+    // Output the mean for the first 100 numbers in the array
+    cout << "Mean: " << fixed << setprecision(1) << Calculate_Mean(mean, sum, arr) << endl;
 
 
     // Close the input and output files
@@ -75,4 +81,49 @@ int main()
     outFile.close();
 
     return 0;
+}
+
+// Calculates the mean of the sum of 100 numbers in the array
+// from the input file
+double Calculate_Mean(double mean, double sum, double arr[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        sum += arr[i];
+    }
+
+    mean = sum / 100;
+
+    return mean;
+}
+
+// Sorts 100 numbers in the array from descending order
+// and finds the median(middle) number of the number set
+double Find_Median(int j, int halfOfElement, double temp, double arr[], double median)
+{
+    // Sorting in ascending order
+    for (int i = 0; i < 100; i++)
+    {
+        for (j = 0; j < 100 - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    median = (arr[halfOfElement] + arr[halfOfElement + 1]) / 2;
+
+    // Print sorted array
+    for (int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+
+    cout << endl;
+
+    return median;
 }
